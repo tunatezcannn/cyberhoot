873d34ac46624a4ad6bda1250ce2898763933116
tunatezcannn <tunatezcann@gmail.com>
2025-04-26 10:37:42 +0300

    init

---
+++ backend/src/main/java/com/touche/cyberhoot/CyberhootApplication.java
@@ -0,0 +1,13 @@
+package com.touche.cyberhoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberhootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberhootApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/touche/cyberhoot/CyberhootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touche.cyberhoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberhootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
